ebe520dc56fcb7cc281a87a6828c9aa8c93a073c
Anders Gustavsson <gustavsson09@hotmail.com>
2017-11-30 11:23:18 +0100

    Created header.php with starting template html structure and created footer.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+	<meta charset="UTF-8">
+  	<meta name="description" content="Roslagens Värme & Fastighetsteknik">
+  	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+  	<meta name="author" content="Anders Gustavsson, Johan Walberg">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Roslagens Värme & Fastighetsteknik</title>
+</head>
+<body>
+
