140ff1db75e6f2d5cabd887825cf33923396984e
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-01 14:28:59 +0100

    Added setup function in functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
-functions.php
+<?php
+/*************************************************
+		Roslagens Värme Setup
+**************************************************/
+functions roslagensvarme_script_enque() {
+	wp_enqueue_style('customstyle', get_template_directory_uri() . 'dist/css/style.css', null, '1.0', 'all' );
+	wp_enqueue_style('googlefonts', '//fonts.googleapis.com/css?family=Josefin+Sans');
+	wp_enqueue_script('main', get_template_directory_uri() . '/dist/js/bundle.js', array('jquery'), '1.0', true);
+}
