afb28ab4c1b13dc15cfe0a6a8bbb3a308f1f93e0
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-01 09:54:40 +0100

    Added googlefonts link in header.php

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Roslagens Värme & Fastighetsteknik</title>
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
 </head>
 <body>
 
