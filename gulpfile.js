461a93b900c5fc25d9e9bbf262e423411c86ccc0
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-01 10:34:43 +0100

    Added task runners for sass and js scripts in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,23 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
-
+// sass
 gulp.task('sass', function(){
   return gulp.src('./src/scss/style.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('./dist/css'))
 });
-
-
-
-gulp.task('watch', ['sass'], function (){
+// concat and bundle js script files
+gulp.task('scripts', function() {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('./dist/js'));
+});
+// gulp task watchers, sass and js scripts
+gulp.task('watch', ['sass', 'scripts'], function (){
   gulp.watch('./src/scss/**/*.scss', ['sass']); 
-  
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  // Other watchers
 });
