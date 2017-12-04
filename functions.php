fdd82f7a1894f9df0a2a98e1d4fa97a59f8c2047
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-04 08:38:09 +0100

    Added comment in functions.php

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 **************************************************/
 functions roslagensvarme_script_enque() {
 	wp_enqueue_style('customstyle', get_template_directory_uri() . 'dist/css/style.css', null, '1.0', 'all' );
+	// Dont use http/https when linking in google fonts, http/https will bug with SSL 
 	wp_enqueue_style('googlefonts', '//fonts.googleapis.com/css?family=Josefin+Sans');
 	wp_enqueue_script('main', get_template_directory_uri() . '/dist/js/bundle.js', array('jquery'), '1.0', true);
 }
