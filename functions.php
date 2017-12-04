0a5030f7714dbaecc30b8c40e9c85fa4deb75e64
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-04 09:34:01 +0100

    Change name on image to scrrenshot-png, created new file theme_setting.php. Added more code fnctions.php

--- functions.php
+++ functions.php
@@ -1,11 +1,37 @@
 <?php
-/*************************************************
-		Roslagens Värme Setup
-**************************************************/
-functions roslagensvarme_script_enque() {
-	wp_enqueue_style('customstyle', get_template_directory_uri() . 'dist/css/style.css', null, '1.0', 'all' );
-	// Dont use http/https when linking in google fonts, http/https will bug with SSL 
-	wp_enqueue_style('googlefonts', '//fonts.googleapis.com/css?family=Josefin+Sans');
+/**************************************************************
+				Theme settings
+**************************************************************/
+require "theme_setting.php";
+/**************************************************************
+				Hooks Setup
+**************************************************************/
+add_action( 'after_setup_theme', 'rvft_blog_setup' );
+
+/**************************************************************
+				Roslagensvarme Setup
+**************************************************************/
+function rvft_blog_setup() {
+	wp_enqueue_style( 'main', get_template_directory_uri() . '/dist/css/style.css', null, '1.0', 'all' );
+	// Make sure to not write http in googlefont link, secerutiy protocall for SSL will not work otherwise
+	wp_enqueue_style('googlefonts', '//fonts.googleapis.com/css?family=Josefin+Sans:400,600');
 	wp_enqueue_script('main', get_template_directory_uri() . '/dist/js/bundle.js', array('jquery'), '1.0', true);
+	register_nav_menu( 'mainmenu', 'Website main navigation' );
+	register_nav_menu( 'footer', 'Website footer navigation' );
+
+	// Custom Logo
+	add_theme_support('custom-logo', array(
+		'width'		=> 600,
+		'height'	=> 85,
+		'flex-width'	=> true,
+		'flex-height'	=> true
+		) );
+	// Register sidebar
+	register_sidebar(array(
+		'name'			=> __("Footer", "rvft"),
+		'id'			=> 'footer',
+		'description'	=> __("Column 1 in footern.", "rvft"),
+		'before_widget'	=> '<div class ="footer footer-col-1">',
+		'after_widget'	=> '</div>'
+		));
 }
-add_action( 'wp_enqueue_script', 'roslagensvarme_script_enque');
---
+++ theme_setting.php
@@ -0,0 +1,8 @@
+<?php
+add_action('admin_menu', 'setup_theme_admin_menus');
+function setup_theme_admin_menus() {
+	$menu_name = _x('Settings', 'rvft');
+	// _x Wp decides on its own if it should be written from context
+	$page_title = _x('Theme settings', 'rvft');
+	add_menu_page($page_title, $menu_name, 'manage_options', 'rvft_settings', 'rvft_settings_page');
+}
