3e7d52a8bafebcf99612f3236c476c68ee2182a4
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-04 13:18:43 +0100

    Created contact us template page

---
+++ page-contactus.php
@@ -0,0 +1,21 @@
+<?php 
+/* 
+* Template Name: Contact us
+*/ 
+?>
+
+<?php get_header(); ?>
+
+<?php
+if(have_posts()) { 
+	while(have_posts()) {
+		
+		the_post();
+		?>
+		<div>Kontakta Oss </div>
+		<?php
+		
+	}
+}
+get_footer();
+?>
