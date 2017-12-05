55c6d9b52d46728fcba5e3fed7a5a3a4929c12fc
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-05 22:14:33 +0100

    Added empty div tag for flexbox positioning of rvft logo and added some comments in page-front.php. Added comments in main.scss

--- page-start.php
+++ page-start.php
@@ -11,12 +11,16 @@
 				
 				the_post();
 				?>
+				<!-- First section on home page -->
 				<div class="rvft-img-outer-container">
 					<div class="rvft-img-inner-container">
 						<img src="<?php the_field('rvftlogga'); ?>" class="rvft-img-logo" alt="Logotype of the company Roslagens Värme & Fastighetsteknik AB" />
 						<h2><?php  echo get_field('logo-text'); ?></h2>
 					<div>
+					<!-- This empty div is flexbox positioning for rvft logo -->
+					<div></div>
 				</div>
+				<!-- Second section, Panasonic pumps -->
 				<div>
 					
 				</div>
