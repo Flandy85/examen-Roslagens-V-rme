519a26c98402417005aa8cbd3b259a50ca0215d3
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-06 22:27:42 +0100

    Added more first setup html structure in page-panasonicpumpsair.php. Also added first setup css for panasonic page in main.scss

--- page-panasonicpumpsair.php
+++ page-panasonicpumpsair.php
@@ -12,6 +12,7 @@
 			
 			the_post();
 			?>
+			<!-- First section, Panasonic and menu -->
 			<div class="panasonic-container">
 				<div class="panasonic-img-container">
 					<img src="<?php the_field('panasonic-image'); ?>" class="panasonic-image" alt="Logotype of the company Panasonic" />
@@ -25,11 +26,22 @@
 					<h3><?php wp_nav_menu(array('theme_location' => 'Panasonic menu')); ?></h3>
 				</div>
 			</div>
+			<!-- Second section, Panasonic infotext and supportlink -->
 			<div class="panasonic-support-container">
 				<div class="panasonic-support-text-container">
 					<h2><?php echo get_field('support-title'); ?></h2>
-					<p><?php echo get_field('support-text'); ?></p>
+					<p><?php echo get_field('support-text'); ?><a href="https://www.aircon.panasonic.eu/SE_sv/contact/" rel="noopener" target="blank"> <span class="Panasonic">Panasonics</span> <span class="pan-customer">kund</span><span class="pan-customer-service">tjänst</span></a> <?php echo get_field('support-text2'); ?></p>
 				</div>	
+			</div>
+			<!-- Third section, Panasonic products -->
+			<div class="panasonic-products-container">
+				<div class="panasonic-products-inner-container">
+					
+				</div>
+				<div class="accessories-container">
+					
+				</div>
+				
 			</div>
 			<?php
 			
