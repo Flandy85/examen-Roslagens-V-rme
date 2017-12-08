efeba743828dbe7b473be7d3f8f9665e3037cc94
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-08 13:20:58 +0100

    Changed text in echo get the field for advanced custom field in page-lgsplitpumps.php

--- page-lgsplitpumps.php
+++ page-lgsplitpumps.php
@@ -35,27 +35,27 @@ if(have_posts()) {
 		<div class="lg-splitpumps-products-container">
 			<div class="lg-splitpumps-products-inner-container">
 				<div class="lg-products">
-					<img src="<?php the_field('waterpump-image1'); ?>" class="waterpump-image" alt="Image of an LG Split system heating air pump" />
+					<img src="<?php the_field('lg-splitpump-image1'); ?>" class="waterpump-image" alt="Image of an LG Split system heating air pump" />
 					<div class="lg-products-text">
-						<h3><?php echo get_field('panawater-product-title1'); ?></h3>
-						<p><?php echo get_field('panawater-product-text1'); ?></p>
-						<p class="lg-link-tag"><?php echo get_field('panawater-product-link1'); ?></p>
+						<h3><?php echo get_field('lgsplit-product-title1'); ?></h3>
+						<p><?php echo get_field('lgsplit-product-text1'); ?></p>
+						<p class="lg-link-tag"><?php echo get_field('lgsplit-product-link1'); ?></p>
 					</div>
 				</div>
 				<div class="lg-products">
-					<img src="<?php the_field('waterpump-image2'); ?>" class="waterpump-image" alt="Image of an LG Split system heating air pump" />
+					<img src="<?php the_field('lg-splitpump-image2'); ?>" class="waterpump-image" alt="Image of an LG Split system heating air pump" />
 					<div class="lg-products-text">
-						<h3><?php echo get_field('panawater-product-title2'); ?></h3>
-						<p><?php echo get_field('panawater-product-text2'); ?></p>
-						<p class="lg-link-tag"><?php echo get_field('panawater-product-link2'); ?></p>
+						<h3><?php echo get_field('lgsplit-product-title2'); ?></h3>
+						<p><?php echo get_field('lgsplit-product-text2'); ?></p>
+						<p class="lg-link-tag"><?php echo get_field('lgsplit-product-link2'); ?></p>
 					</div>
 				</div>
 				<div class="lg-products">
-					<img src="<?php the_field('waterpump-image3'); ?>" class="waterpump-image" alt="Image of an LG Split system heating air pump" />
+					<img src="<?php the_field('lg-splitpump-image2'); ?>" class="waterpump-image" alt="Image of an LG Split system heating air pump" />
 					<div class="lg-products-text">
-						<h3><?php echo get_field('panawater-product-title3'); ?></h3>
-						<p><?php echo get_field('panawater-product-text3'); ?></p>
-						<p class="lg-link-tag"><?php echo get_field('panawater-product-link3'); ?></p>
+						<h3><?php echo get_field('lgsplit-product-title3'); ?></h3>
+						<p><?php echo get_field('lgsplit-product-text3'); ?></p>
+						<p class="lg-link-tag"><?php echo get_field('lgsplit-product-link3'); ?></p>
 					</div>
 				</div>
 			</div>
