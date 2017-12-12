0f064e84cd190426367f8c0b22d2d79a18f051b9
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-12 11:38:22 +0100

    Added first and second structure of html in page-contactus.php. Added first and secondsetup of CSS for contact us oage in main, desktop.scss

--- page-contactus.php
+++ page-contactus.php
@@ -13,27 +13,32 @@ if(have_posts()) {
 		the_post();
 		?>
 		<section>
-			<div class="contactus-container">
-				<div class="contactus-header-container">
-					<h3 class="contactus-title"><?php echo get_field('contactus-title'); ?></h3>
-					<div class="contactus-line"></div>
+			<div class="contactus-flexbox-container">
+				<div class="contactus-container">
+					<div class="contactus-header-container">
+						<h3 class="contactus-title"><?php echo get_field('contactus-title'); ?></h3>
+						<div class="contactus-line"></div>
+					</div>
+					<p><?php echo get_field('contactus-text1'); ?><a href="tel: 08-201201"><?php echo get_field('contactus-phonenumber'); ?></a></p>
+					<p><?php echo get_field('contactus-text2'); ?></p>
+					<!-- Contact form short code from plugin -->
+					<div class="contact-form-container">
+						<?php echo do_shortcode("[wpforms id='158']"); ?>
+					</div>
 				</div>
-				<p><?php echo get_field('contactus-text1'); ?><a href="tel: 08-201201"><?php echo get_field('contactus-phonenumber'); ?></a></p>
-				<p><?php echo get_field('contactus-text2'); ?></p>
-				<!-- Contact form short code from plugin -->
-				<div class="contact-form-container">
-					<?php echo do_shortcode("[wpforms id='158']"); ?>
+				<div class="visitor-container">
+					<div class="visitor-header-container">
+						<h3 class="visitor-title"><?php echo get_field('visitor-title'); ?></h3>
+						<div class="visitor-line"></div>
+					</div>
+					<div class="streetadress-container">
+						<img src="<?php the_field('visitor-image'); ?>" class="visitor-image" alt="Photograph of Roslagens Värme & Fastighetsteknik HQ" />
+						<p><?php echo get_field('visitor-text'); ?><a href="tel: 08-201201"><?php echo get_field('visitor-phonenumber'); ?></a></p>
+					</div>
 				</div>
 			</div>
 		</section>
-		<section>
-			<div>
-				<h3 class="visitor-title"><?php echo get_field('visitor-title'); ?></h3>
-				<div class="visitor-line"></div>
-			</div>
-		</section>
-		<?php
-		
+		<?php	
 	}
 }
 ?>
