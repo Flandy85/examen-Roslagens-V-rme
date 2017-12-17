681cf3846770f8f9f13b9974010f7caf056b9d12
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-17 13:22:48 +0100

    Added autoprefixer in gulpfile.js for webbrowsers

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
 
 // sass
 gulp.task('sass', function(){
@@ -16,8 +17,17 @@ gulp.task('scripts', function() {
     .pipe(concat('bundle.js'))
     .pipe(gulp.dest('./dist/js'));
 });
+
+gulp.task('default', function() {
+	gulp.src('./src/scss/style.scss')
+	.pipe(autoprefixer ({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(gulp.dest('./dist/css'))
+});
 // gulp task watchers, sass and js scripts
-gulp.task('watch', ['sass', 'scripts'], function (){
+gulp.task('watch', ['sass', 'scripts', 'default'], function (){
   gulp.watch('./src/scss/**/*.scss', ['sass']); 
   gulp.watch('src/js/**/*.js', ['scripts']);
   // Other watchers
