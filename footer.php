aceb16917628df6aa4817194aa95ab7c41455c58
Johan Walberg <johanwalberg@outlook.com>
2017-12-18 09:54:59 +0100

    added structures html/css for footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,43 @@
-	<footer>
-		
-	</footer>
-	<?php wp_footer(); ?>
+<footer>
+<div class="footer-container">
+	<!-- Footer Grid BOOTSTRAPGRID -->
+<div class="row">
+	<!-- BOOTSTRAP COLUMN 1 -->
+<div class="col-md-3">
+	<div class="flex-item">
+		<h1>Besöksadress</h1>
+		<p>Rallarbacken 14</p>
+		<p>184 30 Åkersberga</p>
+		<p>Telefon nummer: 08-201201</p>
+	</div>
+</div>
+	<!-- BOOTSTRAP COLUMN 2 -->
+<div class="col-md-6">
+	<div class="flex-item">
+		<h1 class="title-partner">Sammarbetspartners</h1>
+		<div class="flex-item-center">
+			<img class="footer-img" src="img/elon-logo.png">
+			<img class="footer-img" src="img/LG_Logo_svg.png">
+			<img class="footer-img" src="img/Panasonic-Logo.png">
+		</div>
+		<div class="flex-item-center2">
+			<img class="footer-img" src="img/airPatrol-logo.png">
+			<img class="footer-img" src="img/tecknorth-logo.png">
+			<img class="footer-img" src="img/Zave-logo.png">
+		</div>
+	</div>
+</div>
+	<!-- BOOTSTRAP COLUMN 3 -->
+<div class="col-md-3">
+	<div class="flex-item">
+		<h1>Support</h1>
+		<p>Kontakta Oss</p>
+		<p>Att Tänka på inför besök</p>
+	</div>
+</div>
+</div>
+</div>
+</footer>
+<?php wp_footer(); ?>
 </body>
 </html>
