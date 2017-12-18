089618e237c1af92939e39d18ebe87e9684c58f9
Johan Walberg <johanwalberg@outlook.com>
2017-12-18 11:59:53 +0100

    fixed position on numbers on boxes, added fields for WP

--- page-installation.php
+++ page-installation.php
@@ -19,14 +19,16 @@ if(have_posts()) {
         <p><?php echo get_field('placement-text-one'); ?></p>
     </div>
     <div class="box box2">
-        <img class="number-img" src="img/number2.png">
-        <h2>El</h2>
+        <img class="number-img" src="<?php the_field('number2'); ?>">
+        <h2><?php echo get_field('placement-title-two'); ?></h2>
+        <p><?php echo get_field('placement-text-two'); ?></p>
         <p>Pumpen MÅSTE ha ett jordat uttag alternativt , dosa,elskåp,säkring tar vi kan ta ström från</p> 
         <p>Vi montera EJ utan detta.</p> 
     </div>
     <div class="box box3">
-        <img class="number-img" src="img/number3.png">
-        <h2>Utedel</h2>
+        <img class="number-img" src="<?php the_field('number3'); ?>">   
+        <h2><?php echo get_field('placement-title-three'); ?></h2>
+        <p><?php echo get_field('placement-text-three'); ?></p>
         <p>På utedelen bildas det frost när kallare säsong kommer , då kommer utedelen att avfrosta sig</p> 
         <p>själv och släppa ut vattnet som bildas, kan inte vattnet få rinna rakt ut på marken</p> 
         <p>måste ett kondensvatten tråg med värmekabel monteras så man kan leda bort vattnet</p> 
@@ -36,7 +38,7 @@ if(have_posts()) {
         <img class="installation-img" src="<?php the_field('insta-picture1'); ?>">
     </div>
     <div class="box box5">
-        <img class="installation-img" src="img/brev4.png">
+    <img class="installation-img" src="<?php the_field('insta-picture2'); ?>">
     </div>
  
 </div>
