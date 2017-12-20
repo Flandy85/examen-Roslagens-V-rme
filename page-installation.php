94c5c0d49c3705722360f83359fe46869a428c04
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-20 10:27:30 +0100

    Indented html stucture in page-installation.php

--- page-installation.php
+++ page-installation.php
@@ -13,40 +13,39 @@ if(have_posts()) {
 		the_post();
 		?>
 		<div class="main-installation">
-    <div class="box box1">
-        <img class="number-img" src="<?php the_field('number1'); ?>">
-        <h2><?php echo get_field('placement-title-one'); ?></h2>
-        <p><?php echo get_field('placement-text-one'); ?></p>
-    </div>
-    <div class="box box2">
-        <img class="number-img" src="<?php the_field('number2'); ?>">
-        <h2><?php echo get_field('placement-title-two'); ?></h2>
-        <p><?php echo get_field('placement-text-two'); ?></p>
-        <p>Pumpen MÅSTE ha ett jordat uttag alternativt , dosa,elskåp,säkring tar vi kan ta ström från</p> 
-        <p>Vi montera EJ utan detta.</p> 
-    </div>
-    <div class="box box3">
-        <img class="number-img" src="<?php the_field('number3'); ?>">   
-        <h2><?php echo get_field('placement-title-three'); ?></h2>
-        <p><?php echo get_field('placement-text-three'); ?></p>
-        <p>På utedelen bildas det frost när kallare säsong kommer , då kommer utedelen att avfrosta sig</p> 
-        <p>själv och släppa ut vattnet som bildas, kan inte vattnet få rinna rakt ut på marken</p> 
-        <p>måste ett kondensvatten tråg med värmekabel monteras så man kan leda bort vattnet</p> 
-    </div>
+            <div class="box box1">
+                <img class="number-img" src="<?php the_field('number1'); ?>">
+                <h2><?php echo get_field('placement-title-one'); ?></h2>
+                <p><?php echo get_field('placement-text-one'); ?></p>
+            </div>
+            <div class="box box2">
+                <img class="number-img" src="<?php the_field('number2'); ?>">
+                <h2><?php echo get_field('placement-title-two'); ?></h2>
+                <p><?php echo get_field('placement-text-two'); ?></p>
+                <p>Pumpen MÅSTE ha ett jordat uttag alternativt , dosa,elskåp,säkring tar vi kan ta ström från</p> 
+                <p>Vi montera EJ utan detta.</p> 
+            </div>
+            <div class="box box3">
+                <img class="number-img" src="<?php the_field('number3'); ?>">   
+                <h2><?php echo get_field('placement-title-three'); ?></h2>
+                <p><?php echo get_field('placement-text-three'); ?></p>
+                <p>På utedelen bildas det frost när kallare säsong kommer , då kommer utedelen att avfrosta sig</p> 
+                <p>själv och släppa ut vattnet som bildas, kan inte vattnet få rinna rakt ut på marken</p> 
+                <p>måste ett kondensvatten tråg med värmekabel monteras så man kan leda bort vattnet</p> 
+            </div>
    
-    <div class="box box4">
-        <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
-    </div>
-    <div class="box box5">
-    <img class="installation-img installation-img-two" src="<?php the_field('insta-picture2'); ?>">
-    </div>
+            <div class="box box4">
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
+            </div>
+            <div class="box box5">
+                <img class="installation-img installation-img-two" src="<?php the_field('insta-picture2'); ?>">
+            </div>
  
-</div>
+        </div>
 		<?php
 		
 	}
 }
 ?>
 </main>
-<?php get_footer();
-?>
+<?php get_footer();?>
