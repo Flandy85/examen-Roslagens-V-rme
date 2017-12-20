0bca5aa3df8080cc003ac81b7aba9c1f9d1e6181
Anders Gustavsson <gustavsson09@hotmail.com>
2017-12-20 15:10:50 +0100

    Intended htl structure in aboutus, page.employees.php

--- page-aboutus.php
+++ page-aboutus.php
@@ -12,36 +12,36 @@ if(have_posts()) {
 		
 		the_post();
 		?>
-<div class="info-box">
-	<div class="infoo">
-		<h3><?php echo get_field('info-box-title'); ?></h3>
-	</div>
-</div>
-<div class="title-box">
-	<h2 class="title-certifierad"><?php echo get_field('certified-title'); ?></h2>
-</div>
-<div class="row incert-box">
-	<div class="col-md-4 col-md-offset-2 col-md-offset-2 col-xs-offset-2 ">
-		<div class="main-company">
-			<div class="box-incert">
-				<h2 class="title-incert"><?php echo get_field('incert-title'); ?></h2>
-				<div class="line-black"></div>
-				<p><?php echo get_field('incert-text'); ?></p>
-				<div class="col-md-4 col-md-offset-2 incert-imgbox">
-					<img class="incert-img" src="<?php the_field('incert-logo'); ?>">
+		<div class="info-box">
+			<div class="infoo">
+				<h3><?php echo get_field('info-box-title'); ?></h3>
+			</div>
+		</div>
+		<div class="title-box">
+			<h2 class="title-certifierad"><?php echo get_field('certified-title'); ?></h2>
+		</div>
+		<div class="row incert-box">
+			<div class="col-md-4 col-md-offset-2 col-md-offset-2 col-xs-offset-2 ">
+				<div class="main-company">
+					<div class="box-incert">
+						<h2 class="title-incert"><?php echo get_field('incert-title'); ?></h2>
+						<div class="line-black"></div>
+						<p><?php echo get_field('incert-text'); ?></p>
+						<div class="col-md-4 col-md-offset-2 incert-imgbox">
+							<img class="incert-img" src="<?php the_field('incert-logo'); ?>">
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</div>
-<div class="row pro-partners">
-  <div class="col-md-4 col-md-offset-8 col-md-offset-6 col-xs-offset-1">
-  	<h2 class="title-incert"><?php echo get_field('panasonic-title'); ?></h2>
-		<div class="line-white"></div>
-		<p><?php echo get_field('panasonic-text'); ?></p>
-		<img class="panasonic-img" src="<?php the_field('panasonic-logo'); ?>">
-  </div>
-</div>
+		<div class="row pro-partners">
+		  <div class="col-md-4 col-md-offset-8 col-md-offset-6 col-xs-offset-1">
+		  	<h2 class="title-incert"><?php echo get_field('panasonic-title'); ?></h2>
+				<div class="line-white"></div>
+				<p><?php echo get_field('panasonic-text'); ?></p>
+				<img class="panasonic-img" src="<?php the_field('panasonic-logo'); ?>">
+		  </div>
+		</div>
 		<?php
 		
 	}
--- page-employees.php
+++ page-employees.php
@@ -12,50 +12,50 @@ if(have_posts()) {
 		
 		the_post();
 		?>
-<div class="employees-grid center">
-    <div class="grid-cell">
-    	<div class="profile">
-    		<img class="profile-pic profile-pic-odd" src="<?php the_field('employees-pic-one'); ?>">
-    	</div>
-    </div>
-    <div class="grid-cell grid-cell-info">
-    	<div class=""><h2><?php echo get_field('employees-name-one'); ?></h2></div>
-    	<div class=""><h3><i class="fa fa-phone" aria-hidden="true"></i><?php echo get_field('employees-phone-one');?></h3></div>
-    	<div class=""><h3><i class="fa fa-envelope-o" aria-hidden="true"></i><?php echo get_field('employees-mail-one');?></h3></div>
-    </div>
-    <div class="grid-cell">
-    	<div class="profile">
-		<img class="profile-pic profile-pic-even" src="<?php the_field('employees-pic-two'); ?>">
-    	</div>
-    </div>
-    <div class="grid-cell grid-cell-info">
-	<div class=""><h2><?php echo get_field('employees-name-two'); ?></h2></div>
-	<div class=""><h3><i class="fa fa-phone" aria-hidden="true"></i><?php echo get_field('employees-phone-two');?></h3></div>
-	<div class=""><h3><i class="fa fa-envelope-o" aria-hidden="true"></i><?php echo get_field('employees-mail-two');?></h3></div>
-    </div>
-</div>
- <div class="employees-grid center">
-    <div class="grid-cell">
-    	<div class="profile">
-		<img class="profile-pic profile-pic-odd" src="<?php the_field('employees-pic-three'); ?>">
-    	</div>
-    </div>
-    <div class="grid-cell grid-cell-info">
-	<div class=""><h2><?php echo get_field('employees-name-three'); ?></h2></div>
-	<div class=""><h3><i class="fa fa-phone" aria-hidden="true"></i><?php echo get_field('employees-phone-three');?></h3></div>
-	<div class=""><h3><i class="fa fa-envelope-o" aria-hidden="true"></i><?php echo get_field('employees-mail-three');?></h3></div>
-    </div>
-    <div class="grid-cell">
-    	<div class="profile">
-		<img class="profile-pic profile-pic-even" src="<?php the_field('employees-pic-four'); ?>">
-    	</div>
-    </div>
-    <div class="grid-cell grid-cell-info">
-	<div class=""><h2><?php echo get_field('employees-name-four'); ?></h2></div>
-	<div class=""><h3><i class="fa fa-phone" aria-hidden="true"></i><?php echo get_field('employees-phone-four');?></h3></div>
-	<div class=""><h3><i class="fa fa-envelope-o" aria-hidden="true"></i><?php echo get_field('employees-mail-four');?></h3></div>
-    </div>
-</div> 
+        <div class="employees-grid center">
+            <div class="grid-cell">
+            	<div class="profile">
+            		<img class="profile-pic profile-pic-odd" src="<?php the_field('employees-pic-one'); ?>">
+            	</div>
+            </div>
+            <div class="grid-cell grid-cell-info">
+            	<div class=""><h2><?php echo get_field('employees-name-one'); ?></h2></div>
+            	<div class=""><h3><i class="fa fa-phone" aria-hidden="true"></i><?php echo get_field('employees-phone-one');?></h3></div>
+            	<div class=""><h3><i class="fa fa-envelope-o" aria-hidden="true"></i><?php echo get_field('employees-mail-one');?></h3></div>
+            </div>
+            <div class="grid-cell">
+            	<div class="profile">
+        		<img class="profile-pic profile-pic-even" src="<?php the_field('employees-pic-two'); ?>">
+            	</div>
+            </div>
+            <div class="grid-cell grid-cell-info">
+        	<div class=""><h2><?php echo get_field('employees-name-two'); ?></h2></div>
+        	<div class=""><h3><i class="fa fa-phone" aria-hidden="true"></i><?php echo get_field('employees-phone-two');?></h3></div>
+        	<div class=""><h3><i class="fa fa-envelope-o" aria-hidden="true"></i><?php echo get_field('employees-mail-two');?></h3></div>
+            </div>
+        </div>
+         <div class="employees-grid center">
+            <div class="grid-cell">
+            	<div class="profile">
+        		<img class="profile-pic profile-pic-odd" src="<?php the_field('employees-pic-three'); ?>">
+            	</div>
+            </div>
+            <div class="grid-cell grid-cell-info">
+        	<div class=""><h2><?php echo get_field('employees-name-three'); ?></h2></div>
+        	<div class=""><h3><i class="fa fa-phone" aria-hidden="true"></i><?php echo get_field('employees-phone-three');?></h3></div>
+        	<div class=""><h3><i class="fa fa-envelope-o" aria-hidden="true"></i><?php echo get_field('employees-mail-three');?></h3></div>
+            </div>
+            <div class="grid-cell">
+            	<div class="profile">
+        		<img class="profile-pic profile-pic-even" src="<?php the_field('employees-pic-four'); ?>">
+            	</div>
+            </div>
+            <div class="grid-cell grid-cell-info">
+        	<div class=""><h2><?php echo get_field('employees-name-four'); ?></h2></div>
+        	<div class=""><h3><i class="fa fa-phone" aria-hidden="true"></i><?php echo get_field('employees-phone-four');?></h3></div>
+        	<div class=""><h3><i class="fa fa-envelope-o" aria-hidden="true"></i><?php echo get_field('employees-mail-four');?></h3></div>
+            </div>
+        </div> 
 		<?php
 		
 	}
