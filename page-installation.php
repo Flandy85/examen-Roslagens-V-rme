1c3abf33de15ed94b0e64b848d380810832ab77d
Johan Walberg <johanwalberg@outlook.com>
2017-12-29 14:37:04 +0100

    Added new HTML/CSS on page-installation.php. Works in both crome and IE11

--- page-installation.php
+++ page-installation.php
@@ -18,12 +18,27 @@ if(have_posts()) {
                 <h2><?php echo get_field('placement-title-one'); ?></h2>
                 <p><?php echo get_field('placement-text-one'); ?></p>
             </div>
+            <div class="box box-installation-pictures ">
+            <!-- Är till för om vi vill öpnna bilden i nytt fönster
+            <a href="<?php the_field('insta-picture1'); ?>" target="_blank">
+            <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>"></a> -->
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
+            </div>
             <div class="box box2">
                 <img class="number-img" src="<?php the_field('number2'); ?>">
                 <h2><?php echo get_field('placement-title-two'); ?></h2>
                 <p><?php echo get_field('placement-text-two'); ?></p>
                 <p>Pumpen MÅSTE ha ett jordat uttag alternativt , dosa,elskåp,säkring tar vi kan ta ström från</p> 
                 <p>Vi montera EJ utan detta.</p> 
+            </div>
+            <div class="box box-installation-pictures ">
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
             </div>
             <div class="box box3">
                 <img class="number-img" src="<?php the_field('number3'); ?>">   
@@ -33,14 +48,12 @@ if(have_posts()) {
                 <p>själv och släppa ut vattnet som bildas, kan inte vattnet få rinna rakt ut på marken</p> 
                 <p>måste ett kondensvatten tråg med värmekabel monteras så man kan leda bort vattnet</p> 
             </div>
-   
-            <div class="box box4">
+            <div class="box box-installation-pictures ">
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
+                <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
                 <img class="installation-img installation-img-one" src="<?php the_field('insta-picture1'); ?>">
             </div>
-            <div class="box box5">
-                <img class="installation-img installation-img-two" src="<?php the_field('insta-picture2'); ?>">
-            </div>
- 
         </div>
 		<?php
 		
