359ffaa29836ee0d653ddc75629f71017c39c233
Anders Gustavsson <gustavsson09@hotmail.com>
2018-01-02 15:19:21 +0100

    Added a tag around images in footer.php and box shadow effect in footer.scss

--- footer.php
+++ footer.php
@@ -53,20 +53,18 @@
 			<div class="partners-container">
 				<h1 class="title-partner">Sammarbetspartners</h1>
 				<div class="partner-image-container">
-					<img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/elon-logo.png">
-					<img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/LG_Logo_svg.png">
-					<img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/Panasonic-Logo-footer.png">
+					<a href="http://www.elon.se/" rel="noopener" target="blank"><img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/elon-logo.png" alt="Bild på företaget ELON:s loggo "></a>
+					<a href="http://www.lg.com/se/kyla-varmepumpar" rel="noopener" target="blank"><img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/LG_Logo_svg.png" alt="Bild på företaget LG:s loggo "></a>
+					<a href="https://www.aircon.panasonic.eu/SE_sv/" rel="noopener" target="blank"><img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/Panasonic-Logo-footer.png" alt="Bild på företaget Panasonic:s loggo"></a>
 				</div>
 				<div class="partner-image-container">
-					<img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/airPatrol-logo.png">
-					<img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/tecknorth-logo.png">
-					<img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/Zave-logo.png">
+					<a href="https://www.airpatrol.eu/" rel="noopener" target="blank"><img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/airPatrol-logo.png" alt="Bild på företaget airPatrol:s loggo"></a>
+					<a href="https://tnenergi.se/" rel="noopener" target="blank"><img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/tecknorth-logo.png" alt="Bild på företaget technorths:s loggo"></a>
+					<a href="https://varmepumpshopen.se/" rel="noopener" target="blank"><img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/Zave-logo.png" alt="Bild på företaget Zave:s loggo"></a>
 				</div>	
 			</div>
 			<div class="meny-container">
 				<h1>Support</h1>
-				<!-- <p>Kontakta Oss</p>
-				<p>Att Tänka på inför besök</p> -->
 				<div><?php wp_nav_menu(array('theme_location' => 'footer')); ?></div>
 			</div>
 		</div>
