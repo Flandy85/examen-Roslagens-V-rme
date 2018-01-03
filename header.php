e9ff1fda26a14b78cba88a6e417c2d39dbe1b30c
Anders Gustavsson <gustavsson09@hotmail.com>
2018-01-03 23:54:43 +0100

    Added comments header.php

--- header.php
+++ header.php
@@ -7,17 +7,16 @@
   	<meta name="author" content="Anders Gustavsson, Johan Walberg">
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Roslagens Värme & Fastighetsteknik</title>
+	<title>Roslagens Värme & Fastighetsteknik AB</title>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
-	<header class="nav-down">
+	<header class="nav-down"><!-- class nav-down is for making header to go up when users scroll down -->
 		<div class="top-header-container">
-			<img src="<?php echo get_template_directory_uri(); ?>/src/images/Incert-loga.png" class="top-header-logotype" alt="Bild av företaget Incert loggo">
-			<img src="<?php echo get_template_directory_uri(); ?>/src/images/Panapropartners-2.png" class="top-header-logotype" alt="Bild av företaget Panasonic Pro Partners loggo">
-			<img src="<?php echo get_template_directory_uri(); ?>/src/images/LG-logo.png" class="top-header-logotype" alt="Bild av företaget LG Business Partner loggo">
-			
-		</div>
+			<a href="http://www.incert.se/om-incert/" rel="noopener" target="blank"><img src="<?php echo get_template_directory_uri(); ?>/src/images/Incert-loga.png" class="top-header-logotype" alt="Bild av företaget Incert loggo"></a>
+			<a href="https://www.aircon.panasonic.eu/GB_en/happening/how-to-become-a-panasonic-pro-partner/" rel="noopener" target="blank"><img src="<?php echo get_template_directory_uri(); ?>/src/images/Panapropartners-2.png" class="top-header-logotype" alt="Bild av företaget Panasonic Pro Partners loggo"></a>
+			<a href="http://www.lg.com/global/sustainability/business-partner/win-win-growth" rel="noopener" target="blank"><img src="<?php echo get_template_directory_uri(); ?>/src/images/LG-logo.png" class="top-header-logotype" alt="Bild av företaget LG Business Partner loggo"></a>	
+		</div><!-- .top-header-container -->
 		<div class="header-container">
 			<div class="inner-header-container">
 				<a href="<?php echo home_url(); ?>">
@@ -26,7 +25,7 @@
 				<nav class="navigation-menu">
 					<div><?php wp_nav_menu(array('theme_location' => 'mainmenu')); ?></div>
 				</nav><!-- navigation-menu -->
-			</div>
+			</div><!-- .inner-header-container -->
 		</div><!-- header-container -->
 	</header>
 
