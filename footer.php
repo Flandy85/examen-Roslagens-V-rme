4ffc0740f9c478ca5f0d4c3f7fd1aaf3e6c727af
Anders Gustavsson <gustavsson09@hotmail.com>
2018-01-03 15:15:31 +0100

    Added alt attribute to img tag in footer.php. Also added new css for foooter element and in mains.scss

--- footer.php
+++ footer.php
@@ -1,45 +1,3 @@
-<!-- <footer>
-<div class="footer-container"> -->
-	<!-- Footer Grid BOOTSTRAPGRID -->
-<!-- <div class="row"> -->
-	<!-- BOOTSTRAP COLUMN 1 -->
-<!-- <div class="col-md-3">
-	<div class="flex-item">
-		<h1>Besöksadress</h1>
-		<p>Rallarbacken 14</p>
-		<p>184 30 Åkersberga</p>
-		<p>Telefon nummer: 08-201201</p>
-	</div>
-</div> -->
-	<!-- BOOTSTRAP COLUMN 2 -->
-<!-- <div class="col-md-6">
-	<div class="flex-item">
-		<h1 class="title-partner">Sammarbetspartners</h1>
-		<div class="flex-item-center">
-			<img class="footer-img" src="img/elon-logo.png">
-			<img class="footer-img" src="img/LG_Logo_svg.png">
-			<img class="footer-img" src="img/Panasonic-Logo.png">
-		</div>
-		<div class="flex-item-center2">
-			<img class="footer-img" src="img/airPatrol-logo.png">
-			<img class="footer-img" src="img/tecknorth-logo.png">
-			<img class="footer-img" src="img/Zave-logo.png">
-		</div> -->
-		<!-- <div><?php //wp_nav_menu(array('theme_location' => 'footer')); ?></div> -->
-	<!-- </div>
-</div> -->
-	<!-- BOOTSTRAP COLUMN 3 -->
-<!-- <div class="col-md-3">
-	<div class="flex-item">
-		<h1>Support</h1>
-		<div><?php //wp_nav_menu(array('theme_location' => 'footer')); ?></div> -->
-		<!-- <p>Kontakta Oss</p>
-		<p>Att Tänka på inför besök</p> -->
-<!-- 	</div>
-</div>
-</div>
-</div>
-</footer> -->
 	<footer>
 		<div class="footer-container">
 			<div class="adress-container">
@@ -60,7 +18,7 @@
 				<div class="partner-image-container">
 					<a href="https://www.airpatrol.eu/" rel="noopener" target="blank"><img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/airPatrol-logo.png" alt="Bild på företaget airPatrol:s loggo"></a>
 					<a href="https://tnenergi.se/" rel="noopener" target="blank"><img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/tecknorth-logo.png" alt="Bild på företaget technorths:s loggo"></a>
-					<a href="https://varmepumpshopen.se/" rel="noopener" target="blank"><img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/Zave-logo.png"></a>
+					<a href="https://varmepumpshopen.se/" rel="noopener" target="blank"><img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/src/images/Zave-logo.png" alt="Bild på företaget Zave:s loggo"></a>
 				</div>	
 			</div>
 			<div class="meny-container">
