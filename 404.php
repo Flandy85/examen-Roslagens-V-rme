ed7d7d1648841d6cfe107aaa89f427608decc37a
Johan Walberg <johanwalberg@outlook.com>
2018-02-05 15:36:28 +0100

    added files 404, archiv

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: 404 pages (not found)
+*/
+?>
+<?php get_header(); ?>
+<main>
+  <section class="title-section">
+      <h1>404 Sida</h1>
+      <p class="sub-text">Sidan du söker finns inte längre..</p>
+      <?php get_search_form(); ?>
+      <h2><a href="<?php echo home_url(); ?>">Gå Tillbaka Till Startsida.</a></h2>
+    </section>
+    </main>
+  <?php get_footer(); ?>
