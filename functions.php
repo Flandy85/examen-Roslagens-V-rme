23c1921f30ee2dfeee57030567436d0df129d6b5
Johan Walberg <johanwalberg@outlook.com>
2018-02-06 13:15:42 +0100

    added function for if search field is empty in function.php. If epmty it will return to a list of everything on blogg-page

--- functions.php
+++ functions.php
@@ -59,4 +59,13 @@ function rvft_blog_setup() {
 		'flex-height'	=> true
 		) );	
 }
+function SearchFilter($query) {
+	// If 's' request variable is set but empty
+	if (isset($_GET['s']) && empty($_GET['s']) && $query->is_main_query()){
+	   $query->is_search = true;
+	   $query->is_home = false;
+	}
+	return $query;
+ }
+ add_filter('pre_get_posts','SearchFilter');
 ?>
